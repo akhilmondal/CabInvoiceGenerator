bd66324ab8932d5c861fac63f5f833a73526bd71
“akhilmondal” <akhilmandalfamily@gmail.com>
2023-02-21 06:05:04 +0530

    Added: Program to test calculated fare.

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class CabInvoiceGenerator {
+    static final double COST_PER_KM = 10;
+    static final double COST_PER_MIN = 1;
+
+    public double calculateFare(double distance, double time) {
+        double fare = COST_PER_KM * distance + COST_PER_MIN * time;
+        return fare < 5 ? 5 : fare;
+    }
+}
